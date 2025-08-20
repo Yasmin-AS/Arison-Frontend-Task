d9e1370e81973d1419d014c3a107e93dbd00b404
Hey-coder-beep <post6366@gmail.com>
2025-08-20 23:35:44 +0300

    auth on App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,15 +7,15 @@ function App() {
   const location = useLocation();
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 
-  if (!isAuthenticated && !location.pathname.startsWith("/login")) {
+  if (isAuthenticated && !location.pathname.startsWith("/login")) {
     return <Navigate to="/login" replace />;
   }
 
-  if (isAuthenticated && !location.pathname.startsWith("/admin")) {
+  if (!isAuthenticated && !location.pathname.startsWith("/admin")) {
     return <Navigate to="/admin" replace />;
   }
 
-  return <>{isAuthenticated ? <AdminRoutes /> : <AuthRoutes />}</>;
+  return <>{isAuthenticated ? <AdminRoutes /> : <AdminRoutes />}</>;
 }
 
 export default App;
